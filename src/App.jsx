e0272ac28de0c479c3238218c785e6c5b1b623d6
Mo'men Baligh <momenbaligh55@gmail.com>
2024-07-14 22:18:11 +0300

    go

--- src/App.jsx
+++ src/App.jsx
@@ -1,101 +1,82 @@
 import { useState } from 'react';
+
 import './App.css';
 
+const cardDefaultValues = {
+  name: 'Jane Appleseed',
+  number: '0000 0000 0000 0000',
+  month: '00',
+  year: '00',
+  cvc: '000',
+};
+
 export function App() {
-  const [results, setResults] = useState({
-    name: 'Jane Appleseed',
-    number: '0000 0000 0000 0000',
-    month: '00',
-    year: '00',
-    cvc: '000',
+  const [formValues, setFormValues] = useState({
+    name: '',
+    number: '',
+    month: '',
+    year: '',
+    cvc: '',
   });
 
-  const nameInput = document.getElementById('name-input');
-  const nameInputEmpty = document.getElementById('name-input-empty');
-  const numberInput = document.getElementById('number-input');
-  const numberInputEmpty = document.getElementById('number-input-empty');
-  const numberInputInvalid = document.getElementById('number-input-invalid');
-  const monthInput = document.getElementById('month-input');
-  const yearInput = document.getElementById('year-input');
-  const dateInputEmpty = document.getElementById('date-input-empty');
-  const dateInputInvalid = document.getElementById('date-input-invalid');
-  const cvcInput = document.getElementById('cvc-input');
-  const cvcInputEmpty = document.getElementById('cvc-input-empty');
-  const cvcInputInvalid = document.getElementById('cvc-input-invalid');
+  const [errors, setErrors] = useState(null);
 
-  const handleNameChange = e => {
-    setResults({
-      ...results,
-      name: e.target.value,
-    });
-  };
+  const [step, setStep] = useState('continue');
 
-  const handleNumberChange = e => {
-    let dividedNumber = e.target.value.split('');
-    for (let i = 0; i < dividedNumber.length; i = i + 5) {
-      dividedNumber.splice(i, 0, ' ');
-    }
-
-    setResults({
-      ...results,
-      number: dividedNumber,
+  const onChange = e =>
+    setFormValues({
+      ...formValues,
+      [e.target.name]: e.target.value,
     });
-  };
-  const handleMonthChange = e => {
-    setResults({
-      ...results,
-      month: e.target.value,
-    });
-  };
-  const handleYearChange = e => {
-    setResults({
-      ...results,
-      year: e.target.value,
-    });
-  };
-  const handleCvcChange = e => {
-    setResults({
-      ...results,
-      cvc: e.target.value,
-    });
-  };
 
   const handleFormSubmit = e => {
     e.preventDefault();
-    if (nameInput.value.length === 0) {
-      nameInput.style.borderColor = 'red';
-      nameInputEmpty.style.display = 'block';
+
+    setErrors(null);
+
+    if (!formValues.name) {
+      setErrors(errors => ({ ...errors, name: 'EMPTY' }));
+      console.log(errors);
+    } else if (formValues.name.length < 5) {
+      setErrors(errors => ({ ...errors, name: 'INVALID' }));
     }
 
-    if (numberInput.value.length === 0) {
-      numberInput.style.borderColor = 'red';
-      numberInputEmpty.style.display = 'block';
-    } else if (numberInput.value.length !== 16 && +numberInput.value < 10000000000000000) {
-      numberInput.style.borderColor = 'red';
-      numberInputInvalid.style.display = 'block';
+    if (!formValues.number) {
+      setErrors(errors => ({ ...errors, number: 'EMPTY' }));
+    } else if (formValues.number.length !== 16 || formValues.number.match(/^[0-9]+$/) === null) {
+      setErrors(errors => ({ ...errors, number: 'INVALID' }));
     }
 
-    if (monthInput.value.length === 0 || yearInput.value.length === 0) {
-      monthInput.style.borderColor = 'red';
-      yearInput.style.borderColor = 'red';
-      dateInputEmpty.style.display = 'block';
-    } else if (+monthInput.value < 1 || +monthInput.value > 12) {
-      monthInput.style.borderColor = 'red';
-      yearInput.style.borderColor = 'red';
-      dateInputInvalid.style.display = 'block';
+    if (!formValues.month || !formValues.year) {
+      setErrors(errors => ({ ...errors, month: 'EMPTY', year: 'EMPTY' }));
+    } else if (
+      +formValues.month < 1 ||
+      +formValues.month > 12 ||
+      +formValues.year < 25 ||
+      formValues.month.match(/^[0-9]+$/) === null ||
+      formValues.year.match(/^[0-9]+$/) === null
+    ) {
+      setErrors(errors => ({ ...errors, month: 'INVALID', year: 'INVALID' }));
     }
 
-    if (cvcInput.value.length === 0) {
-      cvcInput.style.borderColor = 'red';
-      cvcInputEmpty.style.display = 'block';
-    } else if (cvcInput.value.length !== 3 || cvcInput.value.length !== 4 || +cvcInput.value >= 10000) {
-      cvcInput.style.borderColor = 'red';
-      cvcInputInvalid.style.display = 'block';
+    if (!formValues.cvc) {
+      setErrors(errors => ({ ...errors, cvc: 'EMPTY' }));
+    } else if (
+      (formValues.cvc.length > 2 && formValues.cvc.length < 5) ||
+      formValues.number.match(/^[0-9]+$/) !== null
+    ) {
+      setErrors(errors => ({ ...errors, cvc: 'INVALID' }));
     }
+
+    console.log(errors);
+
+    if (errors !== null) return;
+    setStep('confirm');
   };
 
-  const handleConfirmationSubmit = e => {
+  const onContinue = e => {
     e.preventDefault();
+    setStep('continue');
   };
 
   return (
@@ -103,72 +84,110 @@ export function App() {
       <div className="card-details">
         <div className="card-front">
           <img src="./images/card-logo.svg" alt="" />
-          <p>{results.number}</p>
+          <p>{formValues.number || cardDefaultValues.number}</p>
+
           <div className="bottom">
-            <p>{results.name}</p>
+            <p>{formValues.name || cardDefaultValues.name}</p>
             <p>
-              {results.month}/{results.year}
+              {formValues.month || cardDefaultValues.month}/{formValues.year || cardDefaultValues.year}
             </p>
           </div>
         </div>
         <div className="card-back">
-          <p>{results.cvc}</p>
+          <p>{formValues.cvc || cardDefaultValues.cvc}</p>
         </div>
       </div>
-      <form className="form" onSubmit={handleFormSubmit}>
-        <div className="form-field">
-          <label htmlFor="name">CARDHOLDER NAME</label>
-          <input id="name-input" name="name" onChange={handleNameChange} placeholder="e.g. Jane Appleseed" />
-          <p id="name-input-empty" className="error">
-            Can't be blank!
-          </p>
-        </div>
-        <div className="form-field">
-          <label htmlFor="number">CARD NUMBER</label>
-          <input id="number-input" name="number" onChange={handleNumberChange} placeholder="e.g. 1234 5678 9123 0000" />
-          <p id="number-input-empty" className="error">
-            Can't be blank!
-          </p>
-          <p id="number-input-invalid" className="error">
-            Invalid number
-          </p>
-        </div>
+      {step === 'continue' && (
+        <div className="card-container">
+          <form className="card" onSubmit={handleFormSubmit}>
+            <div
+              className={`form-field name ${
+                errors?.name === 'EMPTY' ? 'empty-error' : '' || errors?.name === 'INVALID' ? 'invalid-error' : ''
+              }`}
+            >
+              <label htmlFor="name">CARDHOLDER NAME</label>
 
-        <div className="form-bottom">
-          <div className="date">
-            <label htmlFor="date">EXP. DATE (MM/YY)</label>
-            <div className="date-inputs">
-              <input id="month-input" name="date" onChange={handleMonthChange} placeholder="MM" />
-              <input id="year-input" name="date" onChange={handleYearChange} placeholder="YY" />
+              <input
+                id="name-input"
+                name="name"
+                value={formValues.name}
+                onChange={onChange}
+                placeholder="e.g. Jane Appleseed"
+              />
+
+              <p className="error-message empty">Can't be blank</p>
+              <p className="error-message invalid">Invalid name</p>
             </div>
-            <div>
-              <p id="date-input-empty" className="error">
-                Can't be blank!
-              </p>
-              <p id="date-input-invalid" className="error">
-                Invalid date
-              </p>
+
+            <div
+              className={`form-field number ${
+                errors?.number === 'EMPTY' ? 'empty-error' : '' || errors?.number === 'INVALID' ? 'invalid-error' : ''
+              }`}
+            >
+              <label htmlFor="number">CARD NUMBER</label>
+
+              <input
+                id="number-input"
+                name="number"
+                value={formValues.number}
+                onChange={onChange}
+                placeholder="e.g. 1234 5678 9123 0000"
+              />
+
+              <p className="error-message empty">Can't be blank</p>
+              <p className="error-message invalid">Wrong format, numbers only</p>
             </div>
-          </div>
-          <div className="form-field cvc">
-            <label htmlFor="cvc">CVC</label>
-            <input id="cvc-input" name="cvc" onChange={handleCvcChange} placeholder="e.g. 123" />
-            <p id="cvc-input-empty" className="error">
-              Can't be blank!
-            </p>
-            <p id="cvc-input-invalid" className="error">
-              Invalid cvc!
-            </p>
-          </div>
+
+            <div
+              className={`form-field date ${
+                errors?.month === 'EMPTY'
+                  ? 'empty-error'
+                  : '' || errors?.year === 'EMPTY'
+                  ? 'empty-error'
+                  : '' || errors?.month === 'INVALID'
+                  ? 'invalid-error'
+                  : '' || errors?.year === 'INVALID'
+                  ? 'invalid-error'
+                  : ''
+              }`}
+            >
+              <label htmlFor="date">EXP. DATE (MM/YY)</label>
+
+              <input className="month" name="month" value={formValues.month} onChange={onChange} placeholder="MM" />
+              <input className="year" name="year" value={formValues.year} onChange={onChange} placeholder="YY" />
+
+              <p className="error-message empty">Can't be blank</p>
+              <p className="error-message invalid">Invalid date</p>
+            </div>
+
+            <div
+              className={`form-field cvc ${
+                errors?.cvc === 'EMPTY' ? 'empty-error' : '' || errors?.cvc === 'INVALID' ? 'invalid-error' : ''
+              }`}
+            >
+              <label htmlFor="cvc-input">CVC</label>
+
+              <input id="cvc-input" name="cvc" value={formValues.cvc} onChange={onChange} placeholder="e.g. 123" />
+
+              <p className="error-message empty">Can't be blank</p>
+              <p className="error-message invalid">Invalid cvc</p>
+            </div>
+            <button>Confirm</button>
+          </form>
         </div>
-        <button>Confirm</button>
-      </form>
-      <form className="confirmation" onSubmit={handleConfirmationSubmit}>
-        <img src="./images/icon-complete.svg" alt="" />
-        <h2>THANK YOU!</h2>
-        <p>We've added your card details</p>
-        <button>Continue</button>
-      </form>
+      )}
+
+      {step === 'confirm' && (
+        <section className="confirmation">
+          <img src="./images/icon-complete.svg" alt="" />
+
+          <h2>THANK YOU!</h2>
+
+          <p>We've added your card details</p>
+
+          <button onClick={onContinue}>Continue</button>
+        </section>
+      )}
     </main>
   );
 }
